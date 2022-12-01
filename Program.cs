605255d9ce8d62d6d579d5b5369c6296bfe009e7
John Cornell <john.cornell@smokeball.com>
2022-12-02 08:17:08 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JC.QuickMove
+{
+    internal class Program
+    {
+        const string BillingBin = "C:\\Versioning\\billing\\bin";
+        const string Binaries = "C:\\Program Files\\Smokeball\\binaries";
+
+        [Flags]
+        private enum Options
+        {
+            None = 0,
+            WPF = 1 << 0,
+            IncludeSharedDesktop = 1 << 8
+        }
+
+        private enum FileAsType { Dll, Pdb, Xml }
+        private enum FileAsDirectory { Bin, Binaries }
+
+        static Options RunOptions { get; set; }
+
+        static void Main(string[] args)
+        {
+            SmokeballProcessController controller = new SmokeballProcessController();
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters detected.");
+                return;
+            }
+            else if (
+                args.Select(a => a.Trim()).FirstOrDefault(a => a == "--help" || a == "?" || a == "/?" || a == "--?") != null)
+            {
+                Console.WriteLine("qw.exe [wpf billing domain (e.g. Fees) moves wpf dll, pdb and xml to binaries]");
+                Console.WriteLine("[--sd] moves Billing.Shared.Desktop ");
+
+                return;
+            }
+            SetupOptions(args);
+
+            controller.Kill();
+
+            if (RunOptions.HasFlag(Options.WPF))
+                MoveBillingWpfFiles(args);
+            if (RunOptions.HasFlag(Options.IncludeSharedDesktop))
+                MoveSharedDesktop();
+
+            controller.Start(SmokeballProcessController.SmokeballProcess.Service);
+        }
+
+        private static void MoveSharedDesktop()
+        {
+            MoveDllAndSupportingFile("Billing.Shared.Desktop");
+        }
+
+        private static void MoveBillingWpfFiles(string[] args)
+        {
+            foreach (var item in args)
+            {
+                if (!item.StartsWith("--"))
+                {
+                    string fileNameBase = $"Billing.{item}.Wpf";
+
+                    MoveDllAndSupportingFile(item);
+                }
+            }
+        }
+
+        private static void MoveDllAndSupportingFile(string fileNameBase)
+        {
+            if (Move(
+                FileAs(fileNameBase, FileAsType.Dll, FileAsDirectory.Bin),
+                FileAs(fileNameBase, FileAsType.Dll, FileAsDirectory.Binaries)
+                ))
+            {
+                Move(
+                    FileAs(fileNameBase, FileAsType.Pdb, FileAsDirectory.Bin),
+                    FileAs(fileNameBase, FileAsType.Pdb, FileAsDirectory.Binaries)
+                    );
+
+                Move(
+                    FileAs(fileNameBase, FileAsType.Xml, FileAsDirectory.Bin),
+                    FileAs(fileNameBase, FileAsType.Xml, FileAsDirectory.Binaries)
+                    );
+            }
+        }
+
+        private static bool Move(string source, string destination)
+        {
+            if (!File.Exists(source))
+            {
+                Console.WriteLine($"{source} does not exists. Ignoring");
+                return false;
+            }
+
+            try
+            {
+                File.Copy(source, destination, true);
+
+                Console.WriteLine($"Moved to {destination}: Last modified {new FileInfo(destination).LastWriteTime}");
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static string FileAs(string file, FileAsDirectory directory) =>
+            Path.Combine(
+                directory == FileAsDirectory.Bin ? (BillingBin) : (Binaries), file);
+
+        private static string FileAs(string fileBase, FileAsType type) =>
+            $"{fileBase}.{type.ToString().ToLower()}";
+
+        private static string FileAs(string fileBase, FileAsType type, FileAsDirectory directory)
+        {
+            return Path.Combine(
+                directory == FileAsDirectory.Bin ? (BillingBin) : (Binaries),
+                FileAs(fileBase, type)
+                );
+        }
+
+        private static void SetupOptions(string[] args)
+        {
+            RunOptions = Options.WPF;
+
+            foreach (var item in args)
+            {
+                if (item.StartsWith("--"))
+                {
+                    switch (item.ToLower())
+                    {
+                        case "--sd":
+                            RunOptions = RunOptions | Options.IncludeSharedDesktop; break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SmokeballProcessController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JC.QuickMove
+{
+    internal class SmokeballProcessController
+    {
+        [Flags]
+        public enum SmokeballProcess { Main = 1, Service = 2 }
+
+        public void Kill(SmokeballProcess processToKill = SmokeballProcess.Main | SmokeballProcess.Service)
+        {
+            if (processToKill.HasFlag(SmokeballProcess.Main)) 
+            {
+                Process process = Process.GetProcessesByName("Smokeball").FirstOrDefault();
+
+                try
+                {
+                    if (process != null)
+                    {
+                        Console.WriteLine("Smokeball.exe stopped");
+                        process.Kill();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unable to find Smokeball.exe");
+                    }
+                }
+                catch(Exception ex) 
+                {
+                    Console.WriteLine($"Error stopping Smokeball.exe : {ex.Message}");
+                }
+            }
+            
+            if (processToKill.HasFlag(SmokeballProcess.Service)) 
+            {
+                ServiceController service = new ServiceController("Smokeball-Windows-Service");
+
+                if (service != null)
+                {
+                    if (service.Status == ServiceControllerStatus.Running || service.Status == ServiceControllerStatus.StartPending)
+                    {
+                        Console.WriteLine("Stopping service");
+                        service.Stop();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find Smokeball Service");
+                }
+            }
+        }
+
+        public void Start(SmokeballProcess processToStart = SmokeballProcess.Main | SmokeballProcess.Service)
+        {
+            if (processToStart.HasFlag(SmokeballProcess.Main))
+            {
+                Process process = Process.GetProcessesByName("Smokeball").FirstOrDefault();
+
+                try
+                {
+                    if (process != null)
+                    {
+                        Console.WriteLine("Smokeball.exe already running");
+                        process.Kill();
+                    }
+                    else
+                    {
+                        Process.Start(new ProcessStartInfo("\"C:\\Program Files\\Smokeball\\binaries\\Smokeball.exe\""));
+                        Console.WriteLine("Smokeball.exe restarted");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error starting Smokeball.exe : {ex.Message}");
+                }
+            }
+
+            if (processToStart.HasFlag(SmokeballProcess.Service))
+            {
+                ServiceController service = new ServiceController("Smokeball-Windows-Service");
+
+                if (service != null)
+                {
+                    if (service.Status == ServiceControllerStatus.Running || service.Status == ServiceControllerStatus.StartPending)
+                    {
+                        service.Start();
+                        Console.WriteLine("Service restarted");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find Smokeball Service");
+                }
+            }
+        }
+    }
+}
